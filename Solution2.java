9aba70843dc6e664883aff69cb5c2a42f642287b
ZnS-20 <mohda698@gmail.com>
2018-03-26 20:59:40 +0530

    Solution 2

---
+++ Solution2.java
@@ -0,0 +1,28 @@
+//Day 2
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution2 {
+
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        double meal_cost = in.nextDouble();
+        int tip_percent = in.nextInt();
+        int tax_percent = in.nextInt();
+        
+        
+        double tip = meal_cost*tip_percent/100;
+        double tax = meal_cost*tax_percent/100;
+        double total = meal_cost+tip+tax;
+        int totalCost=(int) Math.round(total);
+
+        System.out.println("The total meal cost is "+totalCost+" dollars.");
+
+        in.close();
+    }
+}
